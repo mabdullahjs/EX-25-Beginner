025b2d36fe7bcd92b76a9f0a54657edc59ffee50
mabdullahjs <mabdullah2037@outlook.com>
2024-12-01 18:13:40 +0500

    js started

---
+++ 02-javascript/01-Introduction/app.js
@@ -0,0 +1,102 @@
+// console.log("hello Javascript");
+
+// Data types
+// string "dkjhfdsjkfhkjsd" ''
+// number 3123
+// boolean true | false
+
+// "fsdhfsjkdfhk"
+// 3429473.324234
+
+
+
+
+// object
+
+// typeof()
+
+// console.log(typeof("hello world!"));
+// console.log(typeof("20"));
+// console.log(typeof(20));
+// console.log(typeof(true))
+
+
+
+// var 123username x
+// var #username x
+// var user name x
+
+// var username123 x
+// var username 
+// var checkUserAge
+
+
+
+// var username = "Muhammad Abdullah";
+// var firstName = "abdullah"
+// var age = 21;
+// console.log(username)
+// console.log(age);
+
+
+
+// var username = "abdullah";
+// var Username = "faraz";
+// var userName = "usman"
+
+// console.log(username);
+// console.log(userName);
+// console.log(Username);
+
+
+
+
+// // alert("hello world!")
+// var username = "Abdullah"
+
+// // var username = prompt("enter your name");
+// // console.log(typeof(username))
+// // console.log("hello " + username);
+// console.log('hello ' + username);
+// console.log(20 + 20);
+// console.log('20' + '20');
+
+
+
+
+
+// 2 string ka beech ma agar + lagadia to usa concatination.
+
+
+// var break  = "abdullah"
+// console.log(this);
+
+
+
+
+
+
+
+
+// var city = "larkana";
+// console.log(city) //larkana
+// var city = "lahore"
+// console.log(city)//lahore
+
+
+// var city = "Larkana"; //declare | intialize
+// console.log(city);
+// city = "lahore"; // reassign
+// console.log(city);
+// var city = "tandoallahyar" //redeclare
+// console.log(city);
+
+
+
+
+var username;
+console.log(username);
+
+
+
+
