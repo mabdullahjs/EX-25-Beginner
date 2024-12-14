c44f0b7e6b84f4e77a1ee596f1e4fa2d8a8f9883
mabdullahjs <mabdullah2037@outlook.com>
2024-12-14 18:13:24 +0500

    if else

---
+++ 02-javascript/03-If-else/app.js
@@ -0,0 +1,188 @@
+// console.log('hello world!');
+
+// if else statement
+
+// conditional statement
+
+// var num = 10;
+
+// num agar 5 sa bara haa to "you are eligible" 
+// agar num 5 ya 5 sa chota ha to "you are not eligible"
+
+// block
+// scope
+
+// if(num > 5){
+//     console.log("you are eligible")
+// }else {
+//     console.log("you are not eligible");
+
+// }
+
+
+
+
+// gym admission 
+// 18+
+
+// var age = 9;
+
+// if(age >= 18){
+//     console.log("welcome to our gym");
+// }else {
+//     console.log("get out");
+
+// }
+
+
+
+// 18 >= to gym ma admission  miljayega
+// 15 >= 500 zyada do or ajao
+// dafa hojao 
+
+
+
+
+
+// var age = +prompt("enter your age");
+
+// if (age >= 18) {
+//     console.log("welcome to our gym");
+// } else if (age >= 15){
+//     console.log("500 zyada do or ajao");
+// } else{
+//     console.log("dafa hojao");
+    
+// }
+
+
+
+
+// var bankBalance = 200000;
+
+// if(bankBalance > 1000000){
+//     console.log("shaadi mubarak");
+// } else {
+//     console.log("abba nahi maan rha");
+    
+// }
+
+
+
+
+
+// 20 lac ya 20 lac sa bara hoga agar bank balance to shaadi mubarak
+
+// 15 lac sa bara hua to istekhara krwata hain
+
+// 10 lac sa bara hoo istekhara ma naa aya haa liken ma kuch krti ho
+
+// 5 lac abba nahi maan rha
+
+// < 5lac ammi keh rhi hain chat sa chalaang marlongi
+
+// 20,00,000
+
+
+// var bankBalance = +prompt("enter bank balance")
+
+// if(bankBalance >= 2000000){
+//     console.log("shaadi mubarak");
+// } else if(bankBalance >= 1500000){
+//     console.log("istekhara krwata hain");
+
+// } else if(bankBalance >= 1000000){
+//     console.log("istekhara ma naa aya haa liken ma kuch krti ho");
+    
+// } else if(bankBalance >= 500000){
+//     console.log("abba nahi maan rha");
+// } else {
+//     console.log("Out of community nahi krta.");
+    
+// }
+
+
+
+
+
+// var dayOfWk = 'Sun';
+
+// if (dayOfWk === "Sat" || dayOfWk === "Sun"){
+//     console.log("chutti kro rest kro maza kro");
+    
+// }else {
+//     console.log("gareeebo kaam pa hoo!");
+// }
+
+
+
+
+
+
+
+
+
+// even number = divisible by 2
+
+// var num = 11;
+// if(num % 2 === 0){
+//     console.log("even");
+    
+// }else {
+//     console.log("odd");
+    
+// }
+
+// var time = 17
+// if(time >= 9 && time <= 12){
+//     console.log("breakfast time");
+    
+// } else if(time >= 14 && time <= 16){
+//     console.log("lunch time");
+    
+// } else if (time >= 20 && time <= 23){
+//     console.log("dinner time");
+
+// } else {
+//     console.log("Joo bhi laao 2 leta hua ana..");
+    
+// }
+
+
+// logic building
+
+
+
+
+
+// true && true
+// time >= 14 || time <= 16
+// 17
+// lunch
+
+
+
+// agar larki ka liya age 24 honi chahya 24 < nahi allow
+// larka ki 18 honi chahya 18 < larka nahi allow
+
+// var male = 20;
+// var female = 25;
+// if (male >= 18 && female >= 24)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
