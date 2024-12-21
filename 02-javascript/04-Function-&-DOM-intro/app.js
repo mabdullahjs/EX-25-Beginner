f300b1f9689b31356216ae15bdb2f28b66dea2b5
mabdullahjs <mabdullah2037@outlook.com>
2024-12-21 18:58:03 +0500

    functions

---
+++ 02-javascript/04-Function-&-DOM-intro/app.js
@@ -0,0 +1,228 @@
+// console.log("Hello World");
+
+
+// functions
+
+// ()
+
+// alert("hello world")
+
+// piece of code 
+
+
+
+
+
+
+// function greetUser() {
+//     console.log("hello world");
+//     console.log("hello world");
+//     console.log("hello world");
+//     console.log("hello world");
+//     console.log("hello world");
+// }
+
+// greetUser()
+// greetUser()
+
+
+
+
+
+// function hello(){
+//     console.log("hello world!");
+// }
+
+// hello()
+
+
+
+
+// function sum(num1){ //parameter receive
+//     console.log("sum completed" , num1)
+// }
+
+// sum(20)
+// sum(50)
+
+
+
+
+
+
+// function sum(num1 , num2){ //parameter receive
+//     console.log("total ===> " , num1 + num2)
+// }
+
+// sum(30 , 20) // argument send krna
+// sum(70 , 90) // argument send krna
+
+
+
+
+
+
+// nested function
+
+
+// function greet(){
+//     function user(username){
+//         console.log("hello " +  username)
+//     }
+
+//     user("abdullah")
+//     console.log("parent function")
+// }
+
+// greet()
+
+
+// total(20, 30, 40)
+
+
+
+// undefined kiya ha
+// undefined kio derha ha
+// hal kiya ha iska
+
+
+// undefined
+
+// var username = "Abdullah"; // declared // assign
+// var username; // declared // assign
+// console.log(username);
+
+
+// function user(names){
+//     console.log('username ==> ' , names);
+    
+// }
+// user()
+
+// null
+
+// var username = null;
+// console.log(username);
+
+
+
+
+// NaN
+
+// var username = "abdullah";
+// console.log(+username);
+
+
+
+
+
+
+
+
+
+// function greetUser (username , email , age){
+//     console.log("my name is " + username + " and my email is " + email + " and my age is " + age)
+// }
+
+// greetUser("Muhammad Abdullah" , "mabdullah2037@gmail.com" , 21)
+// greetUser("Usman" , "usman@gmail.com" , 17)
+// greetUser("faraz chohan" , "faraz@gmail.com")
+
+
+
+
+// global variable
+// local variable
+
+// var username = "Abdullah" // global variable
+
+// function hello(){
+//     var age = 21 // local variable
+//     console.log("hello world");
+//     console.log(username);
+//     console.log(age);
+// }
+// console.log(age);
+
+// hello()
+
+
+
+
+
+// function hello(username){
+//     console.log("my name is " + username);
+    
+// }
+
+// hello("abdullah")
+// hello("usman")
+// hello("ammmar")
+
+
+
+
+
+
+
+// var num1 = +prompt("enter num one")
+// var num2 = +prompt("enter num two")
+// var operator = prompt("enter operator");
+
+// if(operator === "+"){
+//     console.log("add hoga" , num1 + num2);
+    
+// } else if(operator === "-"){
+//     console.log("minus hoga", num1 - num2);
+    
+// } else if(operator === "*"){
+//     console.log("multiply hoga", num1 * num2);
+    
+// } else if(operator === "/"){
+//     console.log("divide hoga", num1 / num2);
+    
+// } else {
+//     console.log("invalid operator");
+    
+// }
+
+
+
+
+
+
+// var year = 2025;
+
+// if(year % 4 === 0){
+//     console.log("leap year");
+// }else {
+//     console.log("not a leap year");
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+// return
+
+
+
+// var username = prompt("enter your username")
+// console.log(username);
+
+// function total(price1, price2, price3) {
+//     console.log(price1 + price2 + price3);
+
+//     return  price2 + price3
+// }
+
+// var totalAmount = total(20, 40)
+// console.log(totalAmount);
